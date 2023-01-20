b0fad7d9aed8bdfba83eccd271b2b37fa1d58a81
KenGondor <47976054+KenGondor@users.noreply.github.com>
2023-01-20 22:25:11 +0800

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+Set-Alias vim code
+Set-Alias omp oh-my-posh.exe
+
+# Run Install-Module -Name Terminal-Icons -Force
+Import-Module Terminal-Icons
+
+# Run Install-Module -Name z -Force
+
+# Sets PSReadLine
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/easy-term.omp.json" | Invoke-Expression
