fde08a7390b8c08c978686087126f1397b698542
KenGondor <47976054+KenGondor@users.noreply.github.com>
2023-10-14 22:10:15 +0800

    Create moz.ps1

---
+++ moz.ps1
@@ -0,0 +1,3 @@
+# Place this in the same directory as $PROFILE
+# The command to start shell session with this profile is pwsh.exe -NoExit -File <path to this file>
+Start-Process C:\mozilla-build\start-shell.bat
